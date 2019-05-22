8c75896b501915ce55a4f29b97fcbedd4f01243b
RHund <ronald.hund@brockhaus-ag.de>
2019-05-22 10:00:07 +0200

    Initialized project
    Signed-off-by: RHund <ronald.hund@brockhaus-ag.de>

---
+++ src/main/java/erg/rhund/Greeter/GreeterApplication.java
@@ -0,0 +1,13 @@
+package erg.rhund.Greeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreeterApplication.class, args);
+	}
+
+}
